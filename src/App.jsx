b872630338c3cdf7e81c10a66755fa0b0dd6923c
Peeke Kuepers <pkuepers@kaliber.net>
2023-01-05 17:08:31 +0100

    Make smoosh scroll usable for html elements as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useSmooshScroll } from "../lib/react";
+import { useSmooshScroll, useSmooshScrollDocument } from "../lib/react";
 import { Transition } from "../lib/transition";
 import { spring } from "../lib/transitions/spring";
 import "./styles.css";
 
 export default function App() {
-  useSmooshScroll(() => new Transition(spring))
+  useSmooshScrollDocument(() => new Transition(spring))
+  const smooshRef = useSmooshScroll(() => new Transition(spring))
 
   return (
     <div className="App">
@@ -59,6 +60,53 @@ export default function App() {
         of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
         section 1.10.32.
       </p>
+
+      <div ref={smooshRef} className="App-nested">
+        <p>
+          The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+          below for those interested. Sections 1.10.32 and 1.10.33 from "de
+          Finibus Bonorum et Malorum" by Cicero are also reproduced in their
+          exact original form, accompanied by English versions from the 1914
+          translation by H. Rackham.
+        </p>
+
+        <p>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+          If you are going to use a passage of Lorem Ipsum, you need to be sure
+          there isn't anything embarrassing hidden in the middle of text. All
+          the Lorem Ipsum generators on the Internet tend to repeat predefined
+          chunks as necessary, making this the first true generator on the
+          Internet. It uses a dictionary of over 200 Latin words, combined with
+          a handful of model sentence structures, to generate Lorem Ipsum which
+          looks reasonable. The generated Lorem Ipsum is therefore always free
+          from repetition, injected humour, or non-characteristic words etc.
+        </p>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </div>
 
       <h2>Where does it come from?</h2>
       <p>
